f44ef1ff55aeeda7b0be81b306d0b3318c41e3ec
OnurKlc <onurkilic5@gmail.com>
2020-10-31 15:26:25 +0300

    filter restaurants

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,22 @@
 <template>
   <main class="container">
     <AppHeader />
-    <AppRestaurantInfo />
+    <AppRestaurantInfo :data-source="foodData" />
   </main>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader'
 import AppRestaurantInfo from '@/components/AppRestaurantInfo'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     AppHeader,
     AppRestaurantInfo,
+  },
+  computed: {
+    ...mapState(['foodData']),
   },
 }
 </script>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,16 +1,40 @@
 <template>
   <main class="container restaurant">
-    <h1>Restaurants</h1>
-    <AppRestaurantInfo />
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+
+      <AppSelect @change="selectedRestaurant = $event" />
+    </div>
+    <AppRestaurantInfo :data-source="filteredRestaurants" />
   </main>
 </template>
 
 <script>
 import AppRestaurantInfo from '@/components/AppRestaurantInfo'
+import AppSelect from '@/components/AppSelect'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     AppRestaurantInfo,
+    AppSelect,
+  },
+  data() {
+    return {
+      selectedRestaurant: '',
+    }
+  },
+  computed: {
+    ...mapState(['foodData']),
+    filteredRestaurants() {
+      if (this.selectedRestaurant) {
+        return this.foodData.filter((el) => {
+          const name = el.name.toLowerCase()
+          return name.includes(this.selectedRestaurant)
+        })
+      }
+      return this.foodData
+    },
   },
 }
 </script>
